5d8cc6cf7376ac2ca2c01f90f27371784937b430
AbdElrahman Faheem <55807719+abdelrahmanfaheem@users.noreply.github.com>
2025-03-10 15:12:24 +0200

    Update init_dwh.sql

--- scripts/init_dwh.sql
+++ scripts/init_dwh.sql
@@ -1,1 +1,27 @@
 
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'datawarehouse')
+BEGIN
+    CREATE DATABASE datawarehouse;
+END
+GO
+
+USE datawarehouse;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+BEGIN
+    EXEC('CREATE SCHEMA silver');
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+BEGIN
+    EXEC('CREATE SCHEMA gold');
+END
+GO
