8dd5cdc4c9e1c145beb6c3db71c9a260effff2a9
AbdElrahman Faheem <55807719+abdelrahmanfaheem@users.noreply.github.com>
2025-03-10 15:14:07 +0200

    Update init_dwh.sql

--- scripts/init_dwh.sql
+++ scripts/init_dwh.sql
@@ -1,25 +1,45 @@
+/* 
+=========================================
+Script: Create Data Warehouse and Schemas  
+ 
+Description:  
+- Drops the database if it exists.  
+- Creates the database `datawarehouse`.  
+- Creates schemas `bronze`, `silver`, and `gold` if they do not exist.  
+=========================================
+*/
 
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'datawarehouse')
+-- Drop database if it exists  
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'datawarehouse')
 BEGIN
-    CREATE DATABASE datawarehouse;
+    ALTER DATABASE datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE datawarehouse;
 END
 GO
 
+-- Create the database  
+CREATE DATABASE datawarehouse;
+GO
+
+-- Switch to the newly created database  
 USE datawarehouse;
 GO
 
+-- Create schema 'bronze' if it does not exist  
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
 BEGIN
     EXEC('CREATE SCHEMA bronze');
 END
 GO
 
+-- Create schema 'silver' if it does not exist  
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
 BEGIN
     EXEC('CREATE SCHEMA silver');
 END
 GO
 
+-- Create schema 'gold' if it does not exist  
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
 BEGIN
     EXEC('CREATE SCHEMA gold');
