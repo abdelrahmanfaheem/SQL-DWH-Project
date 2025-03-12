1c07a4c447ac7dbec04ce6579c6efbd04918a20e
AbdElrahman Faheem <55807719+abdelrahmanfaheem@users.noreply.github.com>
2025-03-12 13:07:49 +0200

    Update StoredProcedureForBronzeLayer.sql

--- scripts/bronze/StoredProcedureForBronzeLayer.sql
+++ scripts/bronze/StoredProcedureForBronzeLayer.sql
@@ -1,17 +1,14 @@
 /*
 ===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
+Stored Procedure: Load Bronze Layer (Source [ERP & CRM Folder] -> Bronze)
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    This stored procedure loads data into the 'bronze' schema from ERP & CRM Folder | external CSV files. 
     It performs the following actions:
-    - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+		1- Truncates the bronze tables before loading data.
+		2- Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
+ 
 Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
